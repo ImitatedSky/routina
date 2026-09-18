9f81e9598251d40291c328fa0e33dc21577db9bd
Pat Yeh <yehforcode@gmail.com>
2026-09-19 02:48:48 +0800

    Bump Hub to 0.4.1: catalog sections and resumable downloads

--- hub/build.gradle.kts
+++ hub/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.routina.hub"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
 
         vectorDrawables {
             useSupportLibrary = true
